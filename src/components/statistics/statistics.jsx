18e1203e350e813864a153645cb86748149162d4
Reiterovich <reiterovich.andriy@gmail.com>
2023-10-20 12:10:30 +0300

    0.0.3

--- src/components/statistics/statistics.jsx
+++ src/components/statistics/statistics.jsx
@@ -1,12 +1,14 @@
+import css from './statistics.module.css';
+
 export const Statistics = ({ title, stats }) => {
   return (
-    <section className="statistics">
-      {title && <h2 className="title">{title}</h2>}
-      <ul className="stat-list">
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+      <ul className={css.statlist}>
         {stats.map(stat => (
-          <li className="item" key={stat.id}>
-            <span className="label">{stat.label}</span>
-            <span className="percentage"> {stat.percentage}%</span>
+          <li className={css.item} key={stat.id}>
+            <span className={css.label}>{stat.label}</span>
+            <span className={css.percentage}> {stat.percentage}%</span>
           </li>
         ))}
       </ul>
