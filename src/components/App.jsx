43c3fcb13bb88156d5e1bfeb69858634234329f8
Reiterovich <reiterovich.andriy@gmail.com>
2023-10-22 00:37:52 +0300

    add jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,11 @@
 import { User } from './Profile/profile';
 import user from 'data/user.json';
 import data from 'data/data.json';
+import friends from 'data/friends.json';
+import transactions from 'data/transactions.json';
 import { Statistics } from './statistics/statistics';
+import { FriendList } from './friends/friends';
+import { TransactionHistory } from './transactions/transactions';
 
 // Task 1 ---------------
 
@@ -19,7 +23,8 @@ export const App = props => {
         likes={stats.likes}
       />
       <Statistics title="Upload stats" stats={data} />
-      {/* <Statistics stats={data} /> */}
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
