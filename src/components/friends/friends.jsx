3b4dbd83c42a5bc9ccece61bbd24ff9b1dcc01a2
Reiterovich <reiterovich.andriy@gmail.com>
2023-10-25 21:48:35 +0300

    end?

--- src/components/friends/friends.jsx
+++ src/components/friends/friends.jsx
@@ -1,15 +1,5 @@
 import css from './friends.module.css';
-
-const FriendListItem = ({ avatar, name, isOnline }) => {
-  // const status = isOnline ? 'online' : 'offline';
-  return (
-    <li className={css.item}>
-      <span className={isOnline ? `${css.online}` : `${css.offline}`}></span>
-      <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
-      <p className={css.name}>{name}</p>
-    </li>
-  );
-};
+import { FriendListItem } from './friendkistitem';
 
 export const FriendList = ({ friends }) => {
   return (
